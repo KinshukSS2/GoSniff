b576cb146c70f0d20b9eee3db95f97feb87cbae8
KinshukSS2 <kinshuk380@gmail.com>
2025-11-13 11:07:40 +0530

    Add constructor and configuration options

--- detector.go
+++ detector.go
@@ -1,5 +1,6 @@
-package plagiarismpackage plagiarismdetector
+package plagiarism
 
+import "fmt"
 
 const (
 	// N default n-gram size
@@ -28,4 +29,68 @@ type Detector struct {
 	Score           float64
 	Similar         int
 	Total           int
+}
+
+// NewDetector implements the detector interface. Will return a new detector or an error
+// if any of the optional arguments fails.
+func NewDetector(options ...Option) (*Detector, error) {
+	// implement a new detector interface with defaults
+	detector := &Detector{N: N, Lang: LANG, StopWords: StopWords[LANG].([]string)}
+
+	// iterrate over options, apply or return an error on failure
+	for _, opt := range options {
+		if err := opt(detector); err != nil {
+			return nil, err
+		}
+	}
+	// retrun the detecor
+	return detector, nil
+}
+
+// Option applies detector options and returns an error on failure.
+type Option func(*Detector) (err error)
+
+// SetN option sets the detector's n-gram size and must be a positive integer larger than 0,
+// otherwise an error will be returned. The default n-gram size is 8.
+func SetN(n int) Option {
+	return func(p *Detector) (err error) {
+		// check if n-gram size is larger than 0
+		if n > 0 {
+			p.N = n
+			return
+		}
+		// otherwise return an error
+		return fmt.Errorf("illegal n-gram size %d, must be a positive integer larger than 0 (tip consider using values within range 7-16)", n)
+	}
+}
+
+// SetLang option sets the detector's language as well as the stopwords for the specified language.
+// Use ISO 639-1 formatted language codes (https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes).
+// Refer to stopwords.go for all supported languages. If the specified language doesn't exists or
+// has no stopwords (empty []string), will return an error. If you want to use a custom
+// language or a custom stopwords list use SetStopWords option instead.
+func SetLang(lang string) Option {
+	return func(p *Detector) (err error) {
+		// check if language exists and has stopwords
+		if val, ok := StopWords[lang]; ok && val != nil {
+			p.Lang = lang
+			p.StopWords = val.([]string)
+			return
+		}
+		// otherwise return an error
+		return fmt.Errorf("language %s not found or not supported yet (tip consider using a custom stopwords list with SetStopWords option", lang)
+	}
+}
+
+// SetStopWords option will set a custom language and stopword list.
+func SetStopWords(stopWords []string) Option {
+	return func(p *Detector) (err error) {
+		// check if stopwords list is not empty, otherwise return an error
+		if len(stopWords) < 1 {
+			return fmt.Errorf("stopwords list cannot be empty")
+		}
+		p.Lang = "custom"
+		p.StopWords = stopWords
+		return
+	}
 }
